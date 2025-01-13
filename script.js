705eeac467cefe95d521ea52d4cbd5af2770f308
Hea092024 <Hea092024@gmail.com>
2025-01-13 11:21:10 +0100

    added eventListener to checkerBox

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const saveBtn = document.querySelector("#mainButton");
 const checkerBox = document.querySelector("#visInvis");
+
+
+
+
+
+checkerBox.addEventListener("click", () => {
+  console.log (checkerBox.checked)
+   localStorage.setItem("Hidden", checkerBox.checked) 
+})
