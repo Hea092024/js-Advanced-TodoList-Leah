747c3307e4a26b607d1b56156c6128201dfecf26
Hea092024 <Hea092024@gmail.com>
2025-01-13 11:39:16 +0100

    .

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const saveBtn = document.querySelector("#mainButton");
 const checkerBox = document.querySelector("#visInvis");
 
 
-
+checkerBox.checked = localStorage.getItem("Hidden");
 
 
 checkerBox.addEventListener("click", () => {
   console.log (checkerBox.checked)
    localStorage.setItem("Hidden", checkerBox.checked) 
-})
+});
